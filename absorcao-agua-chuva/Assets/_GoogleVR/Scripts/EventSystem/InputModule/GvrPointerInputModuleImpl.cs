665fa75b674a27ac8cad5334b9497c148de105a1
Unknown <alequis01@gmail.com>
2019-03-26 15:23:48 -0300

    Adicionado nova cena com terreno

--- absorcao-agua-chuva/Assets/_GoogleVR/Scripts/EventSystem/InputModule/GvrPointerInputModuleImpl.cs
+++ absorcao-agua-chuva/Assets/_GoogleVR/Scripts/EventSystem/InputModule/GvrPointerInputModuleImpl.cs
@@ -259,9 +259,9 @@ public class GvrPointerInputModuleImpl
               && !(raycastResult.module is GvrPointerGraphicRaycaster)
               && !(raycastResult.module is GvrPointerPhysicsRaycaster))
         {
-            Debug.LogWarning("Using Raycaster (Raycaster: " + raycastResult.module.GetType() +
-            ", Object: " + raycastResult.module.name + "). It is recommended to use " +
-            "GvrPointerPhysicsRaycaster or GvrPointerGrahpicRaycaster with GvrPointerInputModule.");
+            // Debug.LogWarning("Using Raycaster (Raycaster: " + raycastResult.module.GetType() +
+            // ", Object: " + raycastResult.module.name + "). It is recommended to use " +
+            // "GvrPointerPhysicsRaycaster or GvrPointerGrahpicRaycaster with GvrPointerInputModule.");
         }
     }
 
